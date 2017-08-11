54d074a6f761cfc1d33ff272f4434b03a53d8d7c
DESKTOP-0K2C32P\rocky <365570933@qq.com>
2017-08-11 10:08:57 +0800

    用户2的代码更改

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         String str = "Hello World!这是更改后的变量值。";
+        String str2 = "用户2的更改。";
         System.out.println(str);
+        System.out.println(str2);
+
     }
 }
