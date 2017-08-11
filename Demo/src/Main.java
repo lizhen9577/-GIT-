86f2f39c743ace0ee4a9d90487b7bd52bb6a0fcf
DESKTOP-0K2C32P\rocky <365570933@qq.com>
2017-08-11 10:17:00 +0800

    用户1对用户2加的代码进行更改。

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String str = "Hello World!这是更改后的变量值。";
-        String str2 = "用户2的更改。";
+        String str2 = "用户2的更改，用户1再次进行更改。";
         System.out.println(str);
         System.out.println(str2);
 
